e47ac55e1e4235e5b74611934fc7c6c6bb283cd9
Asyff <astraltrinity84@gmail.com>
2026-07-29 00:38:23 +0545

    commit

--- cart/cart.py
+++ cart/cart.py
@@ -13,20 +13,20 @@ class Cart():
     
     def add(self, product,quantity):
         product_id = str(product.id)
-        product_qty = str(quantity)
+        product_qty = int(quantity)
         
         if product_id in self.cart:
-            pass
+            self.cart[product_id] += product_qty
         else:
             #self.cart[product_id] = {'price': str(product.price)} 
-            self.cart[product_id] = int(product_qty)
+            self.cart[product_id] = product_qty
             
             
         self.session.modified = True
         
     
     def __len__(self):
-        return len(self.cart)
+        return sum(self.cart.values())
     
     def get_prods(self):
         product_ids=self.cart.keys()
@@ -48,4 +48,30 @@ class Cart():
         self.session.modified = True
         thing = self.cart
         return thing 
+    
+    def delete(self, product):
+        product_id = str(product)
+        
+        if product_id in self.cart:
+            del self.cart[product_id]
+            
+        self.session.modified = True
+        
+    def cart_total(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)    
+        quantities = self.cart
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale: 
+                       total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+                        
+        
+        return total 
+                    
         
--- cart/views.py
+++ cart/views.py
@@ -2,15 +2,34 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from home.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 # Create your views here.
 
 def cart_summary(request):
     cart = Cart(request)
-    cart_products = cart.get_prods
-    quantities= cart.get_quants
-    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities": quantities})
+    quantities = cart.get_quants() # Make sure to add parentheses () to call your method
+    totals = cart.cart_total()
+    
+    # We will attach quantities and subtotals directly onto the product objects dynamically
+    products_with_data = []
+    for product in cart.get_prods():
+        product_id_str = str(product.id)
+        
+        if product_id_str in quantities:
+            qty = quantities[product_id_str]
+            price = product.sale_price if product.is_sale else product.price
+            
+            # Attach temporary custom attributes onto the object
+            product.qty = qty
+            product.total_price = round(float(price) * qty, 2)
+            
+            products_with_data.append(product)
 
+    return render(request, "cart_summary.html", {
+        "cart_products": products_with_data,
+        "totals": totals
+    })
 def cart_add(request):
     cart= Cart(request)
     if request.POST.get('action')=='post':
@@ -19,21 +38,58 @@ def cart_add(request):
         product = get_object_or_404(Product, id=product_id)
         
         cart.add(product=product, quantity = product_qty)
-        cart_quantity=cart.__len__()
-        response = JsonResponse({'Product Name: ' : product.name})
-        response=JsonResponse({'qty': cart_quantity})
-        return response
+        global_cart_total = cart.__len__()
+        
+        # Get the specific quantity for just THIS product to update its table row
+        specific_product_qty = cart.cart.get(str(product_id), 0)
+
+        response_data = {
+            'product_id': product_id,
+            'qty': specific_product_qty,        # For the specific product input box
+            'global_qty': global_cart_total   # For a nav bar cart counter badge (if you have one)
+        }
+        
+        messages.success(request,("Added to Cart!"))
+        return JsonResponse(response_data)
+
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        
+        # 1. Delete the product from the session state
+        cart.delete(product=product_id)
+        
+        # 2. Grab the newly updated totals for the response payload
+        global_cart_total = cart.__len__()
+        new_grand_total = cart.cart_total()
+
+        # 3. Pass all the information your AJAX success handler needs
+        response_data = {
+            'product_id': product_id,
+            'global_qty': global_cart_total,
+            'grand_total': round(new_grand_total, 2)
+        }
+        
+        return JsonResponse(response_data)
+    
 
 def cart_update(request):
-    cart= Cart(request)
-    if request.POST.get('action')=='post':
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_qty = int(request.POST.get('product_qty'))
-        cart.update(product = product_id, quantity = product_qty)
+        
+        # This calls your Cart class update method
+        cart.update(product=product_id, quantity=product_qty)
        
-        response=JsonResponse({'qty': product_qty})
+        # Get the global cart badge total (sum of all items combined)
+        global_cart_total = cart.__len__()
+
+        response = JsonResponse({
+            'qty': product_qty,
+            'global_qty': global_cart_total
+        })
         return response
     
